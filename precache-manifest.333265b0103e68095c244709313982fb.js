498cc8d6b9bf2ba78294c1f3d94402b146df4b62
Chaz <chesterz0310@hotmail.com>
2020-09-18 15:57:14 +0800

    complete react version

---
+++ precache-manifest.333265b0103e68095c244709313982fb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce83bb3ea87564fcd54033acfa3d2591",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb7c970d1d43f3678870",
+    "url": "/static/css/main.c18b01e5.chunk.css"
+  },
+  {
+    "revision": "fded7e3910f9e93079a6",
+    "url": "/static/js/2.d48c93c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d48c93c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb7c970d1d43f3678870",
+    "url": "/static/js/main.baabd2bc.chunk.js"
+  },
+  {
+    "revision": "981d0a4892b142fd9eb3",
+    "url": "/static/js/runtime-main.22885c66.js"
+  },
+  {
+    "revision": "85a5f98d94653189476d27b82f47ab5f",
+    "url": "/static/media/background_5.85a5f98d.jpg"
+  },
+  {
+    "revision": "ab5d2091b299c1ddbf3772cc141299b0",
+    "url": "/static/media/board.ab5d2091.jpg"
+  },
+  {
+    "revision": "b30cf785055792a69e18a96c54185a01",
+    "url": "/static/media/help.b30cf785.png"
+  },
+  {
+    "revision": "debbba63228fe49765cd1f167cea6218",
+    "url": "/static/media/help_content.debbba63.jpg"
+  }
+]);
